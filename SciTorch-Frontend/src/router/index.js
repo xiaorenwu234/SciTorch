fa613e17194162cfa1cdd7215bc427f791ebd282
xiaorenwu234 <22373330@buaa.edu.cn>
2024-09-12 09:31:11 +0800

    添加首页

--- SciTorch-Frontend/src/router/index.js
+++ SciTorch-Frontend/src/router/index.js
@@ -1,40 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path:'/login',
-        name:'login',
-        component:()=>import('../views/LoginView.vue')
-    },
-    {
-      path:'/register',
-        name:'register',
-        component:()=>import('../views/RegisterView.vue')
-    },
-    {
-      path:'/forget-password',
-        name:'forget-password',
-        component:()=>import('../views/ForgetPasswordView.vue')
-    },
-    {
-      path:'/pdf-detail',
-      name:'pdf-detail',
-      component:()=>import('../views/PdfDetailView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('../views/HomeView.vue')
+        },
+        {
+            path: '/search',
+            name: 'search',
+            component: () => import('../views/SearchView.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('../views/LoginView.vue')
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: () => import('../views/RegisterView.vue')
+        },
+        {
+            path: '/forget-password',
+            name: 'forget-password',
+            component: () => import('../views/ForgetPasswordView.vue')
+        },
+        {
+            path: '/pdf-detail',
+            name: 'pdf-detail',
+            component: () => import('../views/PdfDetailView.vue')
+        }
+    ]
 })
 
 export default router
